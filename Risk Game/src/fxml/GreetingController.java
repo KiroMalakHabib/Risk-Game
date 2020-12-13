6cd8a7ef6c25bb4bcfcccacc3f118a6e0aa35bd9
kirellos.malak <kiromalakhabib@gmail.com>
2020-12-13 13:42:46 +0200

    greeting phase done

--- Risk Game/src/fxml/GreetingController.java
+++ Risk Game/src/fxml/GreetingController.java
@@ -10,6 +10,7 @@ import com.jfoenix.controls.JFXComboBox;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 
 public class GreetingController implements Initializable{
@@ -23,25 +24,62 @@ public class GreetingController implements Initializable{
     @FXML
     private JFXButton startBtn;
     
+    @FXML
+    private Label warningLabel;
+    
+    @FXML
+    private Label warningLabel1;
+    
+    boolean egyptBool = false;
+    boolean usaBool = false;
+    
     @Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
+		agentOneBox.getItems().addAll("Human agent",
+				"Passive agent",
+				"Aggressive agent",
+				"Pacifist agent",
+				"Greedy agent",
+				"A* with heuristic",
+				"Real time A*",
+				"Minimax agent");
+		agentTwoBox.getItems().addAll("Human agent",
+				"Passive agent",
+				"Aggressive agent",
+				"Pacifist agent",
+				"Greedy agent",
+				"A* with heuristic",
+				"Real time A*",
+				"Minimax agent");
 		
 	}
     
     @FXML
     void btnAct(ActionEvent event) {
-
+    	if (!egyptBool && !usaBool) {
+    		warningLabel.setText("You should choose a country first to start a war!");
+    		warningLabel.setVisible(true);
+    	} else if (agentOneBox.getValue() == null || agentTwoBox.getValue() == null) {
+    		warningLabel1.setVisible(true);
+    	} else {
+    		
+    	}
     }
     
     @FXML
     void egyptPressed(MouseEvent event) {
-
+    	egyptBool = true;
+    	usaBool = false;
+    	warningLabel.setText("Egypt is choosen.");
+    	warningLabel.setVisible(true);
     }
 
     @FXML
     void usaPressed(MouseEvent event) {
-
+    	usaBool = true;
+    	egyptBool = false;
+    	warningLabel.setText("USA is choosen.");
+    	warningLabel.setVisible(true);
     }
 
 }
