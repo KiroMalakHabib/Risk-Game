0e7df770a4da60f35d60265555b2c33952e6a09e
MennatullahOsman <40964254+MennatullahOsman@users.noreply.github.com>
2020-12-27 17:14:07 +0200

    update

--- Risk Game/src/model/agents/Agents.java
+++ Risk Game/src/model/agents/Agents.java
@@ -5,7 +5,7 @@ import model.Player;
 
 public interface Agents {
 
-	public void placing_armies(City[] all_cities, Player p, int bonus_armies);
+	public void placing_armies(City[] all_cities, Player p, Player p2,int bonus_armies);
 
 	public boolean attack(City[] all_cities, Player p1, Player p2);
 }
